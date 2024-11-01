fa23bf8d5799fed5033742ad9193bb45464db2d4
tanishabamne <tanisha.bamne1980@gmail.com>
2024-11-01 08:44:35 -0700

    Add files via upload

---
+++ rectangle pattern.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{   cout<<"Rectangle pattern"<<endl;
+    for( int i=1;i<=4;i++){
+        for( int j=1; j<=5; j++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+}
